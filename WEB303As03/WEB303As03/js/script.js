16cda98d6b16fe92b1ff66e17ea68711b94c6510
kushan3 <91295853+kushan3@users.noreply.github.com>
2022-02-13 21:19:55 -0500

    Update script.js

--- WEB303As03/WEB303As03/js/script.js
+++ WEB303As03/WEB303As03/js/script.js
@@ -11,3 +11,28 @@ let first = $.getJSON("team.json").done(function (data) {
 });
 
 console.log("The getjson object: ", first);
+// Using the $.ajax() Method
+	$.ajax({url: "team.json", 
+	beforeSend: function() {
+		var beforeText = $("<p></p>").text("Loading...");
+		$(`div#team`).append(beforeText);
+	},
+	error: function(){
+		
+	},
+	timeout: 5000,                                
+                                                 
+    
+    success: function() {                                
+        $.getJSON('team.json', function(data) {                     
+            $.each(data.members, function (key, val) {
+                console.log("data key: ", key, " and data value: ", val);
+        
+                $('#team').append(`<div><h2>${val.name}</h2></div>
+                    <div><h5>${val.position}</h5></div>
+                    <div><P>${val.bio}</P></div>
+                </div>`);
+            });
+        });}
+    });
+ 
